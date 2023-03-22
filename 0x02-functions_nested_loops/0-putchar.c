f314805922feafe20d982befa86720303cfbb178
Charles Ochieng Omollo <comollo001@gmail.com>
2023-03-22 07:40:00 -0700

    correction

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,28 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
- * beginning of main
+ * print_to_98 - Prints all natural numbers
+ * from n to 98.
  *
- * Return: Always 0.
+ * @n: input number.
+ *
+ * Return: no return.
  */
-int main(void)
+void print_to_98(int n)
 {
-    puts("_putchar")
-    return (0);
+	if (n > 98)
+	{
+		for (; n > 98; n--)
+		{
+			printf("%d, ", n);
+		}
+	}
+	else if (n < 98)
+	{
+		for (; n < 98; n++)
+		{
+			printf("%d, ", n);
+		}
+	}
+	printf("%d\n", n);
 }
